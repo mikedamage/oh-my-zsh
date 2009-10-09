bf50255b5dddea0a2f1deeb92c9b2e3e938d668f
Mike Green <mike.is.green@gmail.com>
2009-10-08 23:24:35 -0400

    moved finder function file to a collection of functions for opening various things

---
+++ custom/openers.zsh
@@ -0,0 +1,28 @@
+# Functions for opening files/folders in various applications
+
+# Opens the specified folder(s) in Finder
+function finder() {
+	if [[ $# -eq 0 ]]; then
+		open $PWD
+	elif [[ $# -eq 1 ]]; then
+		open $1
+	else
+		for dir in $argv; do
+			open $dir
+		done
+	fi
+}
+
+# Opens the specified file(s) in Mozilla Firefox
+function fox() {
+	if [[ $# -eq 0 ]]; then
+		echo "Please supply at least one file to open!"
+	else
+		open -a /Applications/Firefox.app $*
+	fi
+}
+
+# Previews the specified file in QuickLook
+function quicklook() {
+	qlmanage -p $1
+}
