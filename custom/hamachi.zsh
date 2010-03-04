0fdf1519da926c85ecf21fcba5f37d14d920b252
Mike Green <mike.is.green@gmail.com>
2010-03-03 22:16:08 -0500

    added hamachi aliases to custom folder

---
+++ custom/hamachi.zsh
@@ -0,0 +1,8 @@
+# Hamachi Aliases
+# hm: hamachi
+# hml: hamachi list
+# hmgn: hamachi get-nicks
+
+alias hm="/usr/bin/hamachi"
+alias hml="/usr/bin/hamachi list"
+alias hmgn="/usr/bin/hamachi get-nicks"
